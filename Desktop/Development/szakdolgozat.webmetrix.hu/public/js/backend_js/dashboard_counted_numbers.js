4eb61a5521afbbd994b51139477d1996d416a2b5
Csongor <csongor@macbook-MacBook-Pro.local>
2019-02-04 19:10:56 +0100

    01.04.2019

--- Desktop/Development/szakdolgozat.webmetrix.hu/public/js/backend_js/dashboard_counted_numbers.js
+++
@@ -1,6 +0,0 @@
-
-//--------------------SUM of Planned Length of calls ---------------------------------------
-//var visitor = <?php echo json_encode($analytics); ?>;
-
-//var output = document.getElementById('sumplength');
-//output.innerHTML = visitor;
