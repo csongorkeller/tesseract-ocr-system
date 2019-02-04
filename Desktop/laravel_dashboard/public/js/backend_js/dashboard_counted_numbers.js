782a5db8576be8dd2aac29e00a3279d3830bbe54
Csongor <csongor@macbook-MacBook-Pro.local>
2019-02-04 18:11:45 +0100

    2019_01_04

---
+++ Desktop/laravel_dashboard/public/js/backend_js/dashboard_counted_numbers.js
@@ -0,0 +1,6 @@
+
+//--------------------SUM of Planned Length of calls ---------------------------------------
+//var visitor = <?php echo json_encode($analytics); ?>;
+
+//var output = document.getElementById('sumplength');
+//output.innerHTML = visitor;
---
+++ Desktop/laravel_dashboard/resources/views/sum_plength.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Lorem Ipsum</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+ 
+  
+ </head>
+ <body>
+  
+     <div id="sumplength" style="width:750px; height:450px;">
+
+     </div>
+
+
+    
+<script type="text/javascript">
+var visitor = <?php echo $analytics; ?>;
+var output = document.getElementById('sumplength');
+output.innerHTML = visitor;
+
+</script>
+
+
+ </body>
+
+</html>
